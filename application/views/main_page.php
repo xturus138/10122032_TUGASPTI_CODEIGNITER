6b423da83a77cf768f81579b0c60bea204220519
xturus138 <radityaramadhan.138@gmail.com>
2024-11-06 15:58:23 +0700

    Menambahkan Link Href pada main page untuk logo

--- application/views/main_page.php
+++ application/views/main_page.php
@@ -4,6 +4,7 @@
 <html>
 
 <head>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
     <?php include 'inc/head.php'; ?>
 </head>
 
